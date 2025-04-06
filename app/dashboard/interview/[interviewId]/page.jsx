f071eef5982db64f9c7fc8076491e32e8e5a6d64
Akg <ayushkrgupta2003@gmail.com>
2025-04-07 00:03:48 +0530

    webcam size changed

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -33,8 +33,8 @@ function Interview({ params }) {
               onUserMediaError={() => setWebCamEnabled(false)}
               mirrored={true}
               style={{
-                height: 300,
-                width: 300,
+                height: 500,
+                width: 500,
               }}
             />
           ) : (
